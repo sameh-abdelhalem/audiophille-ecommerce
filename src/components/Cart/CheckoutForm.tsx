c3c60380d3965d865684b6756d449eec141287c4
Sameh <sameh.abdelhaleem95@gmail.com>
2023-11-13 21:58:01 +0200

    add all form inputs without styling for the checkout form

--- src/components/Cart/CheckoutForm.tsx
+++ src/components/Cart/CheckoutForm.tsx
@@ -1,3 +1,5 @@
+import Countries from "./Countries";
+
 const CheckoutForm = () => {
   return (
     <div>
@@ -20,18 +22,44 @@ const CheckoutForm = () => {
           </label>
         </div>
         <div>
-          <p>BILLING DETAILS</p>
+          <p>Shipping Info</p>
           <label>
-            Name
-            <input placeholder="Alexei Ward" type="text" name="name" />
+            Address
+            <input
+              placeholder="1137 Williams Avenue"
+              type="text"
+              name="address"
+            />
           </label>
           <label>
-            Email Address
-            <input placeholder="alexei@mail.com" type="email" name="email" />
+            ZIP Code
+            <input placeholder="10001" type="number" name="zip" />
           </label>
           <label>
-            Phone Number
-            <input placeholder="+1 202-555-0136" type="number" name="phone" />
+            Country
+            <Countries />
+          </label>
+        </div>
+        <div>
+          <p>Payment Details</p>
+          <div>
+            Payment Method
+            <label>
+              e-Money
+              <input type="radio" name="method" value="e-Money" />
+            </label>
+            <label>
+              Cash on Delivery
+              <input type="radio" name="method" value="Cash on Delivery" />
+            </label>
+          </div>
+          <label>
+            e-Money Number
+            <input placeholder="238521993" type="number" name="enum" />
+          </label>
+          <label>
+            e-Money PIN
+            <input placeholder="6891" type="number" name="epin" />
           </label>
         </div>
       </form>
