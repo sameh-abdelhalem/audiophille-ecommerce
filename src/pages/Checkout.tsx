f3cd99fdb203af28cb956cfa58a91823d6eafec9
Sameh <sameh.abdelhaleem95@gmail.com>
2023-12-18 21:37:06 +0200

    finalize Checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,11 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import CheckoutForm from "../components/Cart/CheckoutForm";
 import CheckoutSummary from "../components/Cart/CheckoutSummary";
 import classes from "./Checkout.module.scss";
 const CheckoutPage = () => {
+  const navigate = useNavigate();
+
+  const navigateBackHandler = () => {
+    navigate(-1);
+  };
   return (
-    <div className={classes.mainContainer}>
-      <CheckoutForm />
-      <CheckoutSummary />
+    <div>
+      <div className={classes.backBtn}>
+        <span onClick={navigateBackHandler} className={classes.btnText}>
+          Go Back
+        </span>
+      </div>
+      <div className={classes.mainContainer}>
+        <CheckoutForm />
+        <CheckoutSummary />
+      </div>
     </div>
   );
 };
